7bddbd4e68972020492132acee4e3cec73c7a531
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-07-17 17:13:50 +0530

    Swamy/16jun work (#2)
    * Add initial solution and project files for Bethany's Pie Shop Inventory Management
    
    * Add Product class and UnitType enum for inventory management
    
    * Refactor Product class to include properties for Id, Name, and Description with validation
    
    * Refactor Product class to use properties for AmountInStock and IsBelowStockTreshold, and update related methods for consistency

---
+++ src/csharp/oop-fundamentals/demos/BethanysPieShop.InventoryManagement/Product.cs
@@ -0,0 +1,138 @@
+﻿using System.Text;
+
+namespace BethanysPieShop.InventoryManagement;
+
+public class Product
+{
+
+    private int id;
+    private string name = string.Empty;
+    private string? description;
+
+    private int maxItemsInStock = 0;
+
+    public int Id
+    {
+        get { return id; }
+
+        set
+        {
+            id = value;
+        }
+    }
+
+    public string Name
+    {
+        get { return name; }
+
+        set
+        {
+            // [..50] is a C# 8.0 feature called range operator
+            name = value.Length > 50 ? value[..50] : value;
+        }
+    }
+
+    public string? Description
+    {
+        get { return description; }
+
+        set
+        {
+            if (value == null)
+            {
+                description = string.Empty;
+            }
+            else
+            {
+                description = value.Length > 250 ? value[..250] : value;
+
+            }
+        }
+    }
+
+    public UnitType UnitType { get; set; }
+
+    public int AmountInStock { get; private set; }
+
+    public bool IsBelowStockTreshold { get; private set; }
+
+    //ToDo: add price value
+
+    public void UseProduct(int items)
+    {
+        if (items <= AmountInStock)
+        {
+            //use the items
+            AmountInStock -= items;
+
+            UpdateLowStock();
+
+            Log($"Amount in stock updated. Now {AmountInStock} items in stock.");
+        }
+        else
+        {
+            Log($"Not enough items on stock for {CreateSimpleProductRepresentation()}. {AmountInStock} available but {items} requested.");
+        }
+    }
+
+    public void IncreaseStock()
+    {
+        AmountInStock++;
+    }
+
+    private void DecreaseStock(int items, string reason)
+    {
+        if (items <= AmountInStock)
+        {
+            //decrease the stock with the specified number items
+            AmountInStock -= items;
+        }
+        else
+        {
+            AmountInStock = 0;
+        }
+
+        UpdateLowStock();
+
+        Log(reason);
+    }
+
+    public string DisplayDetailsShort()
+    {
+        return $"{id}. {name} \n{AmountInStock} items in stock";
+    }
+
+    public string DisplayDetailsFull()
+    {
+        StringBuilder sb = new();
+        //ToDo: add price here too
+        sb.Append($"{id} {name} \n{description}\n{AmountInStock} item(s) in stock");
+
+        if (IsBelowStockTreshold)
+        {
+            sb.Append("\n!!STOCK LOW!!");
+        }
+
+        return sb.ToString();
+
+    }
+
+    private void UpdateLowStock()
+    {
+        if (AmountInStock < 10)//for now a fixed value
+        {
+            IsBelowStockTreshold = true;
+        }
+    }
+
+    private void Log(string message)
+    {
+        //this could be written to a file
+        Console.WriteLine(message);
+    }
+
+    private string CreateSimpleProductRepresentation()
+    {
+        return $"Product {id} ({name})";
+    }
+}
